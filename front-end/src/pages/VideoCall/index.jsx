8f4186061ed1c65fa04fd2b7e628238900dfcbaf
Duc Khai <57087803+kaisergeX@users.noreply.github.com>
2020-08-20 03:53:58 +0700

    Update Video Call UI

--- front-end/src/pages/VideoCall/index.jsx
+++ front-end/src/pages/VideoCall/index.jsx
@@ -1,5 +1,8 @@
 import React, {useState, useEffect, useLayoutEffect} from "react";
-import {message} from "antd";
+import {useDispatch, useSelector} from "react-redux";
+
+import {message, Tooltip} from "antd";
+import {LoadingOutlined} from "@ant-design/icons";
 
 import calling from "../../assest/image/call/call.png";
 import endCall from "../../assest/image/call/call-disconnected.png";
@@ -14,19 +17,47 @@ import close from "../../assest/image/call/close.png";
 import "./style.css";
 
 const VideoCall = props => {
+    const data = props.match?.params?.direct; //need avatar, name of doctor
+    // const isCallLoad = useSelector(state => state.call.isCallLoad);
+
     const [toggleAction, setToggleAction] = useState(true); //false: calling action, true: ended call action
-    const data = props.match?.params?.direct;
+    const [toggleCam, setToggleCam] = useState(true);
+    const [toggleMic, setToggleMic] = useState(true);
+    const [viewMyFace, setViewMyFace] = useState(false);
+    const [isCallLoad, setIsCallLoad] = useState(true);
 
     const closeWindow = () => {
         window.close();
     };
 
+    const viewAllFace = () => {
+        setViewMyFace(!viewMyFace);
+    };
+
+    const toggleCamera = () => {
+        if (toggleCam) {
+            //logic
+            setToggleCam(false);
+        } else {
+            setToggleCam(true);
+        }
+    };
+
+    const toggleMicro = () => {
+        if (toggleMic) {
+            //logic
+            setToggleMic(false);
+        } else {
+            setToggleMic(true);
+        }
+    };
+
     const actionCall = action => {
         switch (action) {
-            case 0:     //call again
+            case 0: //call again
                 setToggleAction(true);
                 break;
-            case 1:     //end call
+            case 1: //end call
                 setToggleAction(false);
                 break;
 
@@ -35,35 +66,94 @@ const VideoCall = props => {
         }
     };
 
+    //demo flow
+    useEffect(() => {
+        if (toggleAction) {
+            setIsCallLoad(true);
+            setTimeout(() => {
+                setIsCallLoad(false);
+            }, 2000);
+        }
+    }, [toggleAction]);
+
     useEffect(() => {
         console.log(data);
+        setTimeout(() => {
+            setIsCallLoad(false);
+        }, 2000);
     }, []);
 
     return (
         <div className="video-call-wrapper">
             <div className="video-call-container">
-                video call
+                {toggleAction && !isCallLoad ? (
+                    <>
+                        <div className="call-opposite-camera">Doc Face</div>
+                        <div className={`call-my-camera ${viewMyFace ? "call-my-camera-full" : ""}`} onClick={() => viewAllFace()}>
+                            {viewMyFace ? (
+                                <div className="call-hover-action">
+                                    <div className="call-my-camera-action call-action-image call-action-close">
+                                        <img src={close} alt="show-all-face" />
+                                    </div>
+                                    Thu nhỏ
+                                </div>
+                            ) : (
+                                ""
+                            )}
+                            My pretty face
+                        </div>
+                    </>
+                ) : (
+                    <div className="call-waiting">
+                        Doctor avatar & name
+                        {isCallLoad ? (
+                            <div>
+                                <LoadingOutlined /> Đang gọi
+                            </div>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                )}
                 <div className="call-action-bar">
                     {toggleAction ? (
                         <>
-                            <div className="call-action-image">
-                                <img src={cameraOn} alt="cameraOn" />
-                            </div>
-                            <div className="call-action-image">
-                                <img src={micOn} alt="micOn" />
-                            </div>
-                            <div className="call-action-image call-action-off-image" onClick={() => actionCall(1)}>
-                                <img src={endCall} alt="endCall" />
-                            </div>
+                            {toggleCam ? (
+                                <Tooltip title="Tắt Camera">
+                                    <div className="call-action-image" onClick={() => toggleCamera()}>
+                                        <img src={cameraOn} alt="call-camera" />
+                                    </div>
+                                </Tooltip>
+                            ) : (
+                                <Tooltip title="Bật Camera">
+                                    <div className="call-action-image call-action-close" onClick={() => toggleCamera()}>
+                                        <img src={cameraOff} alt="call-camera" />
+                                    </div>
+                                </Tooltip>
+                            )}
+                            <Tooltip title="Kết thúc cuộc gọi" placement="top">
+                                <div className="call-action-image call-action-off-image" onClick={() => actionCall(1)}>
+                                    <img src={endCall} alt="end-call" />
+                                </div>
+                            </Tooltip>
+                            <Tooltip title={(toggleMic ? "Tắt" : "Bật") + " Micro"}>
+                                <div className={`call-action-image ${toggleMic ? "" : "call-action-close"}`} onClick={() => toggleMicro()}>
+                                    <img src={toggleMic ? micOn : micOff} alt="call-micro" />
+                                </div>
+                            </Tooltip>
                         </>
                     ) : (
                         <>
-                            <div className="call-action-image" onClick={() => actionCall(0)}>
-                                <img src={calling} alt="calling" />
-                            </div>
-                            <div className="call-action-image call-action-close" onClick={() => closeWindow()}>
-                                <img src={close} alt="closeWindow" />
-                            </div>
+                            <Tooltip title="Gọi lại">
+                                <div className="call-action-image" onClick={() => actionCall(0)}>
+                                    <img src={calling} alt="calling" />
+                                </div>
+                            </Tooltip>
+                            <Tooltip title="Đóng">
+                                <div className="call-action-image call-action-close" onClick={() => closeWindow()}>
+                                    <img src={close} alt="closeWindow" />
+                                </div>
+                            </Tooltip>
                         </>
                     )}
                 </div>
