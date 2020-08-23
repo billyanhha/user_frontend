f886c5209f47de7ef506c5c46cad8ec7a455f1b5
peanutnbt <nbaotan98@gmail.com>
2020-08-23 18:02:40 +0700

    edit env

--- front-end/src/components/Footer/index.jsx
+++ front-end/src/components/Footer/index.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
             <div className="footer-logo"><img alt='logo' src={logo} /></div>
             <Row gutter={[3, 12]} >
                 <Col xs={24} sm={12} md={12}>
-                    <p><b>Điện thoại</b> 02838409652</p>
-                    <p><b>Hotline </b> 1900636893</p>
+                    <p><b>Điện thoại</b> 123456789</p>
+                    <p><b>Hotline </b> 987654321</p>
                     <p><b>Hỗ trợ </b> support@ikemen.com</p>
-                    <p>Copyright © 2018 IKEMEN</p>
+                    <p>Copyright © 2020 IKEMEN</p>
                 </Col>
                 <Col xs={24} sm={12} md={4}>
                     <p><b>Dịch vụ</b></p>
